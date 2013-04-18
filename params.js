1c3683e065866cca2288c15af8b2dab8983bd599
kayakyakr <kayakyakr@gmail.com>
2013-04-17 19:09:34 -0500

    Update and rename params.json to params.js

---
+++ params.js
@@ -0,0 +1,10 @@
+THEMES = [
+  'Sad Songs',
+  '1994',
+  'Songs for Robots',
+  'Rainy Days and Mondays',
+  'Best Song Ever',
+  'Describe your opponent',
+  'Describe the mod',
+  'The day of the week'
+]
